46827d9f717b0633b14854f0fe0cf9eabcdf2bf0
Michal Iwanejko <michaliwanejko16@gmail.com>
2026-05-29 14:07:43 +0200

    States 4: Landing States

--- AeroSimulator/Core/States/DescentState.cs
+++ AeroSimulator/Core/States/DescentState.cs
@@ -1,6 +1,55 @@
 namespace AeroSimulator.Core.States;
 
-public class DescentState
+using System;
+using System.Collections.Generic;
+using AeroSimulator.Core.Aircraft;
+
+public class DescentState : IAircraftState
 {
-    
+    public string StateName => "DESCENT";
+    public string StateDescription => "Schodzenie z wysokości przelotowej i przygotowanie do lądowania.";
+    public ConsoleColor StateColor => ConsoleColor.DarkMagenta;
+    public IReadOnlyList<string> AllowedActions => new List<string> { "Land", "Abort", "HandleEmergency" };
+
+    public void OnEnter(Aircraft ctx)
+    {
+        // Ustawienie przepustnicy na 30%
+        ctx.FlightData.Throttle = 0.3; 
+        
+        // Rozpoczęcie wysuwania klap (inkrementalnie)
+        // ctx.Hydraulics.SetFlaps(FlapPosition.Position1); // (Zależnie od pełnej implementacji Hydraulics)
+    }
+
+    public void Update(Aircraft ctx, double deltaT)
+    {
+        // Zmniejszanie wysokości i prędkości (symulacja fizyki zniżania)
+        double descentRate = ctx.Config.NormalDescentFtMin / 60.0; // stopy na sekundę
+        ctx.FlightData.Altitude -= descentRate * deltaT;
+        ctx.FlightData.Speed -= 5.0 * deltaT; // Stopniowa utrata prędkości
+
+        // Warunek automatycznego przejścia do lądowania
+        if (ctx.FlightData.Altitude < 3000.0 && ctx.FlightData.Speed < 180.0)
+        {
+            Land(ctx);
+        }
+    }
+
+    public void Land(Aircraft ctx)
+    {
+        ctx.TransitionTo(new LandingState());
+    }
+
+    public void Abort(Aircraft ctx)
+    {
+        // Go-around (przerwanie podejścia)
+        ctx.TransitionTo(new ClimbState());
+    }
+
+    public void HandleEmergency(Aircraft ctx) => ctx.TransitionTo(new EmergencyState());
+
+    public void TakeOff(Aircraft ctx) { }
+    public void Cruise(Aircraft ctx) { }
+    public void Descend(Aircraft ctx) { } // Już schodzimy
+
+    public void OnExit(Aircraft ctx) { }
 }
--- AeroSimulator/Core/States/LandingState.cs
+++ AeroSimulator/Core/States/LandingState.cs
@@ -1,6 +1,85 @@
 namespace AeroSimulator.Core.States;
 
-public class LandingState
+using System;
+using System.Collections.Generic;
+using AeroSimulator.Core.Aircraft;
+
+public class LandingState : IAircraftState
 {
-    
+    // Pola prywatne zalecone w specyfikacji
+    private double _ilsDeviation;
+    private double _touchdownSpeed;
+    // private LandingPhase _phase; // Opcjonalnie do podziału na Approach/Flare/Rollout
+
+    public string StateName => "LANDING";
+    public string StateDescription => "Podejście finałowe (ILS) i przyziemienie.";
+    public ConsoleColor StateColor => ConsoleColor.Magenta;
+    public IReadOnlyList<string> AllowedActions => new List<string> { "Abort", "HandleEmergency" };
+
+    public void OnEnter(Aircraft ctx)
+    {
+        // Pełne klapy i wypuszczenie podwozia
+        // ctx.Hydraulics.DeployGear();
+        // ctx.Hydraulics.SetFlaps(FlapPosition.Full);
+        
+        // Zapisanie optymalnej prędkości przyziemienia
+        _touchdownSpeed = ctx.Config.VRSpeedKts * 0.9; // Uproszczona logika
+    }
+
+    public void Update(Aircraft ctx, double deltaT)
+    {
+        // 1. Śledzenie ścieżki schodzenia (Glideslope)
+        ctx.FlightData.Altitude -= (700.0 / 60.0) * deltaT; // Schodzenie ok 700 ft/min
+        
+        // 2. Obsługa wiatru bocznego (Crosswind) - symulacja wibracji/znoszenia
+        // ctx.FlightData.Heading += crosswindDrift * deltaT;
+
+        // 3. Asymetryczny opór z palącego się skrzydła (najtrudniejsza część)
+        if (ctx.DamageModel.AsymmetricDragActive)
+        {
+            // Pilot ma dużo trudniej z wyrównaniem samolotu przed pasem
+            ctx.FlightData.ApplyAsymmetricDrift(ctx.DamageModel.DriftDegPerSec * 1.5, deltaT); 
+        }
+
+        // 4. Flare (Wyrównanie przed przyziemieniem - poniżej 50 stóp)
+        if (ctx.FlightData.Altitude <= 50.0 && ctx.FlightData.Altitude > 0)
+        {
+            ctx.FlightData.PitchAngleDeg = 3.0; // Delikatne zadarcie nosa
+            ctx.FlightData.Throttle = 0.0;      // Przepustnica na zero (Idle)
+        }
+
+        // 5. Touchdown (Przyziemienie)
+        if (ctx.FlightData.Altitude <= 0)
+        {
+            ctx.FlightData.Altitude = 0;
+            
+            // Rejestracja zdarzenia LandingCompletedEvent byłaby tutaj wysyłana na EventBus
+            
+            // Zakończenie lotu i przejście do stanu na ziemi
+            ctx.TransitionTo(new GroundState());
+        }
+    }
+
+    public void Abort(Aircraft ctx)
+    {
+        // Go-around (Odejście na drugi krąg)
+        ctx.FlightData.Throttle = 1.0; // Pełny ciąg
+        // ctx.Hydraulics.RetractGear(); // Schowanie podwozia
+        // ctx.Hydraulics.SetFlaps(FlapPosition.Position1);
+        
+        ctx.TransitionTo(new ClimbState());
+    }
+
+    public void HandleEmergency(Aircraft ctx) => ctx.TransitionTo(new EmergencyState());
+
+    public void TakeOff(Aircraft ctx) { }
+    public void Cruise(Aircraft ctx) { }
+    public void Descend(Aircraft ctx) { }
+    public void Land(Aircraft ctx) { } // Już lądujemy
+
+    public void OnExit(Aircraft ctx)
+    {
+        // Reset ewentualnych parametrów ILS
+        _ilsDeviation = 0;
+    }
 }
