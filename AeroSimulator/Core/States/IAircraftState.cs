fc1ed7e32a8eda7cb1cf8c2abe1bb26b6109bb80
Michal Iwanejko <michaliwanejko16@gmail.com>
2026-05-29 13:56:55 +0200

    States 1: Deklaracja stanów w IAircraftState.cs + Implementajca stanów w Aircraft.cs

--- AeroSimulator/Core/States/IAircraftState.cs
+++ AeroSimulator/Core/States/IAircraftState.cs
@@ -1,6 +1,30 @@
 namespace AeroSimulator.Core.States;
 
-public class IAircraftState
+using System;
+using System.Collections.Generic;
+using AeroSimulator.Core.Aircraft;
+
+/// <summary>
+/// Interfejs reprezentujący abstrakcyjny stan samolotu w maszynie stanów.
+/// </summary>
+public interface IAircraftState
 {
-    
+    // Właściwości stanu
+    string StateName { get; }
+    string StateDescription { get; }
+    ConsoleColor StateColor { get; }
+    IReadOnlyList<string> AllowedActions { get; }
+
+    // Metody akcji gracza/kontrolera delegowane do stanu
+    void TakeOff(Aircraft ctx);
+    void Cruise(Aircraft ctx);
+    void Descend(Aircraft ctx);
+    void Land(Aircraft ctx);
+    void HandleEmergency(Aircraft ctx);
+    void Abort(Aircraft ctx);
+
+    // Metody cyklu życia stanu
+    void Update(Aircraft ctx, double deltaT);
+    void OnEnter(Aircraft ctx);
+    void OnExit(Aircraft ctx);
 }
