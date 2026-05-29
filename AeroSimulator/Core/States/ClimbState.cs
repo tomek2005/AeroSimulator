d0c82a5d33bf68522bbaefef9b7287eda186c670
Michal Iwanejko <michaliwanejko16@gmail.com>
2026-05-29 14:05:45 +0200

    States 3: In-Flight States

--- AeroSimulator/Core/States/ClimbState.cs
+++ AeroSimulator/Core/States/ClimbState.cs
@@ -1,6 +1,49 @@
 namespace AeroSimulator.Core.States;
 
-public class ClimbState
+using System;
+using System.Collections.Generic;
+using AeroSimulator.Core.Aircraft;
+
+public class ClimbState : IAircraftState
 {
+    private double _targetAltitude;
+    private double _climbRate;
+
+    public string StateName => "CLIMB";
+    public string StateDescription => "Wznoszenie na wysokość przelotową.";
+    public ConsoleColor StateColor => ConsoleColor.Cyan;
+    public IReadOnlyList<string> AllowedActions => new List<string> { "Cruise", "HandleEmergency" };
+
+    public void OnEnter(Aircraft ctx)
+    {
+        // Chowanie podwozia i klap
+        // W pełnej wersji systemu może to wymagać wywołania na: ctx.Hydraulics
+        
+        // Pobieramy docelową wysokość i prędkość wznoszenia z konfiguracji lub telemetryki
+        _targetAltitude = ctx.FlightData.TargetAltitude > 0 ? ctx.FlightData.TargetAltitude : ctx.Config.MaxAltitudeFt;
+        _climbRate = ctx.Config.MaxClimbRateFtMin / 60.0; // konwersja na stopy na sekundę
+    }
+
+    public void Update(Aircraft ctx, double deltaT)
+    {
+        // Zwiększanie wysokości
+        ctx.FlightData.Altitude += _climbRate * deltaT;
+
+        // Kiedy osiągniemy pułap -> Przejście do lotu przelotowego
+        if (ctx.FlightData.Altitude >= _targetAltitude)
+        {
+            ctx.FlightData.Altitude = _targetAltitude; // Wyrównanie dla pewności
+            ctx.TransitionTo(new CruiseState());
+        }
+    }
+
+    public void Cruise(Aircraft ctx) => ctx.TransitionTo(new CruiseState());
     
+    public void HandleEmergency(Aircraft ctx) => ctx.TransitionTo(new EmergencyState());
+
+    public void TakeOff(Aircraft ctx) { }
+    public void Descend(Aircraft ctx) { }
+    public void Land(Aircraft ctx) { }
+    public void Abort(Aircraft ctx) { }
+    public void OnExit(Aircraft ctx) { }
 }
--- AeroSimulator/Core/States/CruiseState.cs
+++ AeroSimulator/Core/States/CruiseState.cs
@@ -1,6 +1,47 @@
 namespace AeroSimulator.Core.States;
 
-public class CruiseState
+using System;
+using System.Collections.Generic;
+using AeroSimulator.Core.Aircraft;
+
+public class CruiseState : IAircraftState
 {
+    public string StateName => "CRUISE";
+    public string StateDescription => "Lot przelotowy z włączonym autopilotem.";
+    public ConsoleColor StateColor => ConsoleColor.Blue;
+    public IReadOnlyList<string> AllowedActions => new List<string> { "Descend", "HandleEmergency" };
+
+    public void OnEnter(Aircraft ctx)
+    {
+        // Włączenie autopilota (tryb ALT_HOLD + HDG)
+        // Zakładając dostęp do systemu autopilota, np. ctx.GetSystem(SystemType.Autopilot)
+    }
+
+    public void Update(Aircraft ctx, double deltaT)
+    {
+        // W pełnej wersji spalanie paliwa robi ctx.FuelSystem.Burn(...), 
+        // ale musimy sprawdzać poziom paliwa: Jeśli spadnie poniżej 5% -> Awaria
+        if (ctx.FlightData.FuelRemainingPercent() < 5.0)
+        {
+            HandleEmergency(ctx);
+            return;
+        }
+
+        // BARDZO WAŻNE: Obsługa asymetrycznego oporu (Asymmetric Drag) z DamageModel
+        if (ctx.DamageModel.AsymmetricDragActive)
+        {
+            // Aplikujemy znoszenie samolotu (Heading drift) za pomocą dedykowanej metody we FlightData
+            ctx.FlightData.ApplyAsymmetricDrift(ctx.DamageModel.DriftDegPerSec, deltaT);
+        }
+    }
+
+    public void Descend(Aircraft ctx) => ctx.TransitionTo(new DescentState());
     
+    public void HandleEmergency(Aircraft ctx) => ctx.TransitionTo(new EmergencyState());
+
+    public void TakeOff(Aircraft ctx) { }
+    public void Cruise(Aircraft ctx) { }
+    public void Land(Aircraft ctx) { }
+    public void Abort(Aircraft ctx) { }
+    public void OnExit(Aircraft ctx) { }
 }
